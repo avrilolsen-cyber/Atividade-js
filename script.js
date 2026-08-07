79e1497600f75126f3964e8301d459edd21c0a80
AVRIL ARGANARAZ OLSEN <avril_olsen@estudante.sesisenai.org.br>
2026-08-06 21:01:33 -0300

    fix a 2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // para "Texto alterado com sucesso!"
 //
 // Dica: use document.getElementById() e .textContent
-document.getElementsById("btn1").addEventListener("click",function(){
+document.getElementById("btn1").addEventListener("click",function(){
     document.getElementById("texto1").textContent = "texto alterado com seucesso!"
 })
 
@@ -21,7 +21,10 @@ document.getElementsById("btn1").addEventListener("click",function(){
 // Ao clicar no botao, mude a cor do texto #texto2 para azul
 //
 // Dica: use .style.color = "blue"
+document.getElementById("btn2").addEventListener("click", () => {
+    document.getElementById("texto2").style.color = "blue";
 
+});
 
 
 // ---------- Exercicio 3 ----------
